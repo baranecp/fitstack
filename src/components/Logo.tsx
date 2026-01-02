8f74e88847b7120ee9b6cd589ece516fe005342b
baranecp <baranec.dev@gmail.com>
2026-01-02 13:47:35 +0100

    feat: add Logo component for branding display

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div>
+      <span className='text-3xl tracking-tight dark:text-white '>
+        Fit<span className='text-emerald-500'>Stack</span>
+      </span>
+    </div>
+  );
+};
+
+export default Logo;
