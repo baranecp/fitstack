5ba92d08c644a33f4dde37520cff089475587fa8
baranecp <baranec.dev@gmail.com>
2026-01-02 14:51:04 +0100

    feat: update globals.css and tailwind.config.ts for improved theming and color management

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,67 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        border: "var(--border)",
-        input: "var(--input)",
-        ring: "var(--ring)",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "var(--primary-foreground)",
-        },
-        secondary: {
-          DEFAULT: "var(--secondary)",
-          foreground: "var(--secondary-foreground)",
-        },
-        destructive: {
-          DEFAULT: "var(--destructive)",
-          foreground: "var(--destructive-foreground)",
-        },
-        muted: {
-          DEFAULT: "var(--muted)",
-          foreground: "var(--muted-foreground)",
-        },
-        accent: {
-          DEFAULT: "var(--accent)",
-          foreground: "var(--accent-foreground)",
-        },
-        popover: {
-          DEFAULT: "var(--popover)",
-          foreground: "var(--popover-foreground)",
-        },
-        card: {
-          DEFAULT: "var(--card)",
-          foreground: "var(--card-foreground)",
-        },
-
-        "brand-emerald": {
-          50: "#ecfdf5",
-          100: "#d1fae5",
-          400: "#34d399",
-          500: "#10b981", // Main
-          600: "#059669",
-          950: "#022c22",
-        },
-        "brand-blue": {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          400: "#60a5fa",
-          500: "#3b82f6", // Main
-          600: "#2563eb",
-          950: "#172554",
-        },
-        "brand-purple": {
-          50: "#faf5ff",
-          100: "#f3e8ff",
-          500: "#a855f7",
-          600: "#9333ea",
-          950: "#3b0764",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: "class",
