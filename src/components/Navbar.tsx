619e03f57f82f20786f21a6e674beb6df64b8622
baranecp <baranec.dev@gmail.com>
2026-01-03 17:26:36 +0100

    feat: implement Navbar component with Logo, NavItems, ThemeChanger, and UserInfo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Logo from "./Logo";
+import NavItems from "./nav-items";
+import ThemeChanger from "./ThemeChanger";
+import UserInfo from "./UserInfo";
+
+const Navbar = () => {
+  return (
+    <nav className='border-b border-border'>
+      <div className='max-w-7xl mx-auto px-6 py-5 flex gap-3 items-center justify-between'>
+        <Logo />
+        <NavItems />
+        <span className='flex gap-6'>
+          <ThemeChanger />
+          <UserInfo />
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
