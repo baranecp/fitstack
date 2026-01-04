87ebd7576faacadf35a10934324bf3c7e7a60735
baranecp <baranec.dev@gmail.com>
2026-01-04 11:43:34 +0100

    fix: remove unnecessary padding from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import UserInfo from "./UserInfo";
 const Navbar = () => {
   return (
     <nav className='border-b border-border'>
-      <div className='max-w-7xl mx-auto px-6 py-5 flex gap-3 items-center justify-between'>
+      <div className='max-w-7xl mx-auto py-5 flex gap-3 items-center justify-between'>
         <Logo />
         <NavItems />
         <div className='flex gap-6'>
