43bd4efd06d9899ac33f1c96160f1e9059285575
baranecp <baranec.dev@gmail.com>
2026-01-06 14:23:21 +0100

    feat: add mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,24 @@ import UserInfo from "./UserInfo";
 
 const Navbar = () => {
   return (
-    <nav className='border-b border-border'>
-      <div className='max-w-7xl mx-auto py-5 flex gap-3 items-center justify-between'>
-        <Logo />
-        <NavItems />
-        <div className='flex gap-6'>
-          <ThemeChanger />
-          <UserInfo />
+    <>
+      <nav className='hiddne md:block border-b border-border'>
+        <div className='max-w-7xl mx-auto py-5 flex gap-3 items-center justify-between'>
+          <Logo />
+          <NavItems />
+          <div className='flex gap-6'>
+            <ThemeChanger />
+            <UserInfo />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <nav className='md:hidden border-b border-border'>
+        <div className='max-w-7xl mx-auto py-5 flex gap-3 items-center justify-between'>
+          <NavItems />
+        </div>
+      </nav>
+    </>
   );
 };
 
