44692aa3e4d90b1a3f5021c9549dff2e89e378fe
baranecp <baranec.dev@gmail.com>
2026-01-16 13:18:40 +0100

    feat(training-plans): support creating workout plans with exercises

--- src/app/training-plans/actions.ts
+++ src/app/training-plans/actions.ts
@@ -1,12 +1,30 @@
 "use server";
 
-import { createWorkoutPlan } from "@/services/workout-plans";
+import { createWorkoutPlanWithExercises } from "@/services/workout-plans";
 import { revalidatePath } from "next/cache";
 
 export const createTrainingPlan = async (formData: FormData) => {
   const workoutName = formData.get("name") as string;
   const workoutDescription = formData.get("description") as string;
   const userId = 1;
-  await createWorkoutPlan(userId, workoutName, workoutDescription);
+  const exercises = [];
+  let index = 0;
+
+  while (formData.has(`exercises[${index}][id]`)) {
+    exercises.push({
+      id: Number(formData.get(`exercises[${index}][id]`)),
+      sets: Number(formData.get(`exercises[${index}][sets]`)),
+      reps: formData.get(`exercises[${index}][reps]`) as string,
+      notes: formData.get(`exercises[${index}][notes]`) as string,
+    });
+    index++;
+  }
+
+  await createWorkoutPlanWithExercises(
+    userId,
+    workoutName,
+    workoutDescription,
+    exercises
+  );
   revalidatePath("/training-plans");
 };
