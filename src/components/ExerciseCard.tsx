ee2255d9eb63d7bbb358bb6c24b7ba170c683f7f
baranecp <baranec.dev@gmail.com>
2026-01-29 11:19:22 +0100

    refactor(exercises): make ExerciseCard reusable with props

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,7 +1,21 @@
 import { Pen, Trash2 } from "lucide-react";
 import { Button } from "./ui/button";
 
-export const ExerciseCard = () => {
+export const ExerciseCard = ({
+  exerciseName,
+  sets,
+  reps,
+  weight,
+  notes,
+  onEdit,
+}: {
+  exerciseName: string;
+  sets: number;
+  reps: string;
+  weight: string;
+  notes: string | null;
+  onEdit: () => void;
+}) => {
   return (
     <article className='flex flex-col gap-4 p-4 border border-border-card hover:border-border-card-hover rounded-2xl'>
       <header className='flex items-center justify-between'>
@@ -11,11 +25,15 @@ export const ExerciseCard = () => {
             aria-label='Exercise order'>
             1
           </span>
-          <h3 className='font-semibold'>Squats</h3>
+          <h3 className='font-semibold'>{exerciseName}</h3>
         </div>
 
         <div className='flex gap-1'>
-          <Button variant='ghost' className='p-2' aria-label='Edit exercise'>
+          <Button
+            variant='ghost'
+            className='p-2'
+            aria-label='Edit exercise'
+            onClick={onEdit}>
             <Pen />
           </Button>
           <Button
@@ -29,19 +47,19 @@ export const ExerciseCard = () => {
       <dl className='flex gap-6'>
         <div>
           <dt className='text-sm text-muted-foreground'>Sets</dt>
-          <dd className='font-medium'>3</dd>
+          <dd className='font-medium'>{sets}</dd>
         </div>
         <div>
           <dt className='text-sm text-muted-foreground'>Reps</dt>
-          <dd className='font-medium'>10</dd>
+          <dd className='font-medium'>{reps}</dd>
         </div>
         <div>
           <dt className='text-sm text-muted-foreground'>Weight</dt>
-          <dd className='font-medium'>225 lb</dd>
+          <dd className='font-medium'>{weight}</dd>
         </div>
       </dl>
       <section className='p-4 bg-blue-300 rounded-2xl'>
-        <p>Heavy day</p>
+        <p>{notes}</p>
       </section>
     </article>
   );
